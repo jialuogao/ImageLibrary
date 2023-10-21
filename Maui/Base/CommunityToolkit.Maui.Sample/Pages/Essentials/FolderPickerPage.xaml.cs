103305c0ab16c9ed7fb9f0b4bbb5dc6743030b83
Jialuo Gao <jialuogao@microsoft.com>
2023-10-20 20:57:07 -0700

    Display local file images in view

--- Maui/Base/CommunityToolkit.Maui.Sample/Pages/Essentials/FolderPickerPage.xaml.cs
+++ Maui/Base/CommunityToolkit.Maui.Sample/Pages/Essentials/FolderPickerPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Maui.Sample.ViewModels.Essentials;
+using Image = Microsoft.Maui.Controls.Image;
 
 namespace CommunityToolkit.Maui.Sample.Pages.Essentials;
 
@@ -7,5 +8,18 @@ public partial class FolderPickerPage : BasePage<FolderPickerViewModel>
 	public FolderPickerPage(FolderPickerViewModel viewModel) : base(viewModel)
 	{
 		InitializeComponent();
+	}
+
+	void HandleAddButtonClicked(object? sender, System.EventArgs e)
+	{
+		UniformItemsLayout_MaxColumns6.Children.Add(
+			new Image
+			{
+				Margin = new Thickness(5),
+				WidthRequest = 100,
+				HeightRequest = 100,
+				Aspect = Aspect.AspectFill,
+				Source = ImageSource.FromFile(@"C:\src\ImageLibrary\Maui\Base\TestData.ignore\SustainabilityBadge.jpg"),
+			});
 	}
 }
